6b3aa50c81c7184bf67747f62cd4e5f61c10536f
John Blischak <jdblischak@gmail.com>
2014-10-02 16:51:07 -0500

    Assess quality with FastQC, map reads with Subread, and count reads with samtools.

---
+++ process.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+FASTQGZ=$1
+
+SUBREAD=/mnt/lustre/home/jdblischak/src/subread-1.4.4-Linux-x86_64/bin
+FASTQC=/mnt/lustre/data/tools/FastQC
+
+echo "Unzip fastq file"
+FASTQ=${FASTQGZ%.gz}
+zcat $FASTQGZ > $FASTQ
+
+echo "Assess quality with FastQC"
+$FASTQC/fastqc $FASTQ
+
+echo "Map reads"
+BAM=${FASTQ%fastq}bam
+$SUBREAD/subread-align -i data/genome/combined -r $FASTQ --BAMoutput > $BAM
+
+echo "Assess mapping"
+COUNTS=${FASTQ%fastq}counts.txt
+samtools view -F 4 $BAM | cut -f 3 | sort | uniq -c > $COUNTS
+
+echo "Remove intermediate file"
+rm $FASTQ
