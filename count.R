dae7f1ee6ad73f9032b427d4f23ed90d3fa4b304
John Blischak <jdblischak@gmail.com>
2014-10-02 21:40:34 -0500

    Count unknown reads. Does not work if files have different numbers of sequences mapped.

--- count.R
+++ count.R
@@ -1,5 +1,6 @@
 files <- list.files("data", "counts.txt", full.name = TRUE)
 seqs <- scan(files[1], what = "character", quiet = TRUE)[c(FALSE, TRUE)]
+seqs <- sub("\\*", "unknown", seqs)
 counts <- matrix(nrow = length(seqs), ncol = length(files))
 rownames(counts) <- seqs
 colnames(counts) <- files
@@ -11,7 +12,9 @@ for (i in seq_along(files)) {
 total_human <- sum(counts[grep("chr", rownames(counts)), ])
 total_ercc <- sum(counts[grep("ERCC", rownames(counts)), ])
 total_phix <- sum(counts["phix", ])
+total_unknown <- sum(counts["unknown", ])
 
 cat(sprintf("Human reads:\t%8d\n", total_human))
 cat(sprintf("ERCC reads:\t%8d\n", total_ercc))
 cat(sprintf("PhiX reads:\t%8d\n", total_phix))
+cat(sprintf("Unknown reads:\t%8d\n", total_unknown))
