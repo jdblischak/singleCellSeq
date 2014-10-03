d10eee4bfa7861bdbba1fa6a25edc99c9966901b
John Blischak <jdblischak@gmail.com>
2014-10-02 21:55:16 -0500

    Change counting so that do not need to worry about missing count data in some of the files.

--- count.R
+++ count.R
@@ -1,18 +1,20 @@
 files <- list.files("data", "counts.txt", full.name = TRUE)
-seqs <- scan(files[1], what = "character", quiet = TRUE)[c(FALSE, TRUE)]
-seqs <- sub("\\*", "unknown", seqs)
-counts <- matrix(nrow = length(seqs), ncol = length(files))
-rownames(counts) <- seqs
-colnames(counts) <- files
-for (i in seq_along(files)) {
-  f <- scan(files[i], what = "character", quiet = TRUE)
-  counts[, i] <- as.numeric(f[c(TRUE, FALSE)])
+total_human <- 0
+total_ercc <- 0
+total_phix <- 0
+total_unknown <- 0
+for (f in files) {
+  raw <- scan(f, what = "character", quiet = TRUE)
+  counts <- as.numeric(raw[c(TRUE, FALSE)])
+  names(counts) <- raw[c(FALSE, TRUE)]
+  total_human <- total_human + sum(counts[grep("chr", names(counts))])
+  total_ercc <- total_ercc + sum(counts[grep("ERCC", names(counts))])
+  total_phix <- total_phix + sum(counts[grep("phix", names(counts))])
+  total_unknown <- total_unknown + sum(counts[grep("\\*", names(counts))])
 }
 
-total_human <- sum(counts[grep("chr", rownames(counts)), ])
-total_ercc <- sum(counts[grep("ERCC", rownames(counts)), ])
-total_phix <- sum(counts["phix", ])
-total_unknown <- sum(counts["unknown", ])
+stopifnot(total_human + total_ercc + total_phix + total_unknown
+          == 4e6 * length(files))
 
 cat(sprintf("Human reads:\t%8d\n", total_human))
 cat(sprintf("ERCC reads:\t%8d\n", total_ercc))
