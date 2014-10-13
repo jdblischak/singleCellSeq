46884ec071a5aea9c9eca226f76ad5f9698951f6
John Blischak <jdblischak@gmail.com>
2014-10-12 21:55:06 -0500

    Save symlinks to fastq in directory called seqs.

--- link_fastq.sh
+++ link_fastq.sh
@@ -2,7 +2,7 @@
 
 # Create symlinks to fastq files in /rawdata
 
-mkdir -p data/fastq
+mkdir -p data/seqs
 
 LANE1=/rawdata/Illumina_Runs/140923_SN_0795_0387_AHA9RUADXX/Unaligned4/Undetermined_indices/Sample_lane1
 LANE2=/rawdata/Illumina_Runs/140923_SN_0795_0387_AHA9RUADXX/Unaligned4/Undetermined_indices/Sample_lane2
@@ -10,5 +10,5 @@ LANE2=/rawdata/Illumina_Runs/140923_SN_0795_0387_AHA9RUADXX/Unaligned4/Undetermi
 for fq in $LANE1/*fastq.gz $LANE2/*fastq.gz
 do
   #echo $fq
-  ln -s $fq /home/jdblischak/single-cell-seq/data/fastq/`basename $fq`
+  ln -s $fq /home/jdblischak/single-cell-seq/data/seqs/`basename $fq`
 done
