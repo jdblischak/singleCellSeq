05bc37846d611251f650fe8c74370d3edc14c208
John Blischak <jdblischak@gmail.com>
2014-10-13 13:01:42 -0500

    Add steps to process UMI's with umitools.

--- process.sh
+++ process.sh
@@ -12,13 +12,23 @@ zcat $FASTQGZ > $FASTQ
 echo "Assess quality with FastQC"
 $FASTQC/fastqc $FASTQ
 
+echo "Trim UMI's from 5' end of reads"
+umitools trim --end 5 $FASTQ NNNNNGGG --verbose > $FASTQ.trim
+
 echo "Map reads"
 BAM=${FASTQ%fastq}bam
-$SUBREAD/subread-align -i data/genome/combined -r $FASTQ --BAMoutput --trim5 8 > $BAM
+$SUBREAD/subread-align -i data/genome/combined -r $FASTQ.trim --BAMoutput > $BAM
 
 echo "Assess mapping"
 COUNTS=${FASTQ%fastq}counts.txt
 samtools view $BAM | cut -f 3 | sort | uniq -c > $COUNTS
 
+echo "Sort and index mapped reads"
+samtools sort $BAM ${BAM%bam}sorted
+samtools index ${BAM%bam}sorted.bam
+
+echo "Save one read per UMI"
+umitools rmdup ${BAM%bam}sorted.bam $BAM.umi > ${BAM%bam}umi.diffs.bed
+
 echo "Remove intermediate file"
-rm $FASTQ
+rm $FASTQ $FASTQ.trim
