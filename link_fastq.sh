4253a9b306e30d840559dcebed8d8e24970e7fba
John Blischak <jdblischak@gmail.com>
2014-10-12 20:46:34 -0500

    Create symlinks to fastq files in rawdata.

---
+++ link_fastq.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Create symlinks to fastq files in /rawdata
+
+mkdir -p data/fastq
+
+LANE1=/rawdata/Illumina_Runs/140923_SN_0795_0387_AHA9RUADXX/Unaligned4/Undetermined_indices/Sample_lane1
+LANE2=/rawdata/Illumina_Runs/140923_SN_0795_0387_AHA9RUADXX/Unaligned4/Undetermined_indices/Sample_lane2
+
+for fq in $LANE1/*fastq.gz $LANE2/*fastq.gz
+do
+  #echo $fq
+  ln -s $fq /home/jdblischak/single-cell-seq/data/fastq/`basename $fq`
+done
