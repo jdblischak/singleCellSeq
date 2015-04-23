d66c5972fe537dcbe933bcb964aebe6d2b2b2b4b
John Blischak <jdblischak@gmail.com>
2015-04-22 22:14:46 -0500

    Quality trim the reads from the 3' end with sickle.
    https://github.com/najoshi/sickle

---
+++ code/sickle.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+FILE=$1
+BASE=`basename ${FILE%.fastq.gz}`
+OUTDIR=sickle
+
+mkdir -p $OUTDIR
+
+if [ ! -s $FILE ]
+then
+  echo "File is missing or empty: $FILE"
+  exit
+fi
+
+if [ -s $OUTDIR/$BASE.sickle.fastq.gz ]
+then
+  echo "Output file already exists: $OUTDIR/$BASE.sickle.fastq.gz"
+  exit 64
+fi
+
+# Run sickle only cutting from the 3' end
+sickle se -f <(zcat $FILE) -t sanger -o $OUTDIR/$BASE.sickle.fastq.gz -x -g
+
+zcat $OUTDIR/$BASE.sickle.fastq.gz | grep "@" | wc -l > $OUTDIR/$BASE.sickle.count.txt
