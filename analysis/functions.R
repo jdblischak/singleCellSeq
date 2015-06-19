a1ee85ad75fc8c4257d43b7c05f319b10d96fbe8
John Blischak <jdblischak@gmail.com>
2015-06-19 13:41:19 -0500

    Improve PCA plots.
    *  Include percent variance explained in axis labels.
    *  Allow multiple aesthetics, e.g. color, size, shape

--- analysis/functions.R
+++ analysis/functions.R
@@ -8,26 +8,51 @@ run_pca <- function(x, retx = TRUE, center = TRUE, scale = TRUE) {
 }
 
 
-plot_pca <- function(x, pcx = 1, pcy = 2, explained = NULL, color = NULL,
-                     shape = NULL, size = NULL) {
+# Plot PCA results.
+#
+# Args:
+#   x: numeric matrix of PCs
+#   pcx: PC to plot on x-axis (default: 1)
+#   pcy: PC to plot on y-axis (default: 2)
+#   explained: numeric vector of fractions of variance explained by each PC
+#   metadata: data frame or matrix that contains the metadata used to annotate
+#             the plot
+#   color, shape, size: column name of metadata used to pass column to ggplot
+#                       aesthetic
+#   factors: character vector which contains the column names of metadata that
+#            need to be explcitly converted to a factor
+plot_pca <- function(x, pcx = 1, pcy = 2, explained = NULL, metadata = NULL,
+                     color = NULL, shape = NULL, size = NULL, factors = NULL) {
   library("ggplot2")
-#   library("testit")
-#   assert("PC and metadata  matrix have same number of rows.",
-#          nrow(pc) == nrow(metadata))
-  plot_data <- cbind(x, color, shape, size)
-  plot_data <- as.data.frame(plot_data)
+  library("testit")
+
+  # Prepare data frame to pass to ggplot
+  if (!is.null(metadata)) {
+    assert("PC and metadata have same number of rows.",
+           nrow(x) == nrow(metadata))
+    plot_data <- cbind(x, metadata)
+    plot_data <- as.data.frame(plot_data)
+    # Convert numeric factors to class "factor"
+    for (f in factors) {
+      plot_data[, f] <- as.factor(plot_data[, f])
+    }
+  } else {
+    plot_data <- as.data.frame(x)
+  }
+  # Prepare axis labels
+  if (!is.null(explained)) {
+    assert("Number of PCs differs between x and explained.",
+           length(explained) == ncol(x))
+    xaxis <- sprintf("PC%d (%.2f%%)", pcx, round(explained[pcx] * 100, 2))
+    yaxis <- sprintf("PC%d (%.2f%%)", pcy, round(explained[pcy] * 100, 2))
+  } else {
+     xaxis <- paste0("PC", pcx)
+     yaxis <- paste0("PC", pcy)
+  }
+  # Plot
   p <- ggplot(plot_data, aes_string(x = paste0("PC", pcx),
                                     y = paste0("PC", pcy))) +
-    geom_point()
-  if (!is.null(color)) {
-    p <- p + geom_point(aes(color = as.factor(color)))
-  }
-  if (!is.null(shape)) {
-    p <- p + geom_point(aes(shape = as.factor(shape)))
-  }
-
-
-    # geom_point()
-    # geom_point(aes(col = col, shape = shape, size = size))
-  print(p)
+    geom_point(aes_string(color = color, shape = shape, size = size)) +
+    labs(x = xaxis, y = yaxis)
+  p
 }
