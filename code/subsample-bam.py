61adf400c6a59394bb5778388105a7fc20fdd661
John Blischak <jdblischak@gmail.com>
2015-06-30 07:14:33 -0500

    Update subsampler to work on a single bam file. Arguments include a seed for the random number generator and the number of reads to subsample.

--- code/subsample-bam.py
+++ code/subsample-bam.py
@@ -1,13 +1,4 @@
-# John Blischak
-# 27 Sep 2012
-
-# MACS gives WARNINGS if the read counts between the treatment and control
-# samples are drastically different because this could skew the FDR
-# calculations (eg if the control has many more reads, it is more likely
-# to have a peak by chance). Using the parameter --to-small to scale
-# the larger data set towards the smaller one does not remove this
-# warning. To overcome this, this script subsamples from the larger bam
-# file the exact number of reads of the smaller sample.
+#!/usr/bin/env python
 
 # This script was modeled after the subsampling script I found on SeqAnswers
 # for fasta/fastq files. I remodeled it for use with bam files by
@@ -16,71 +7,66 @@
 
 ################################################################################
 
-import pysam
-import random
 import sys
+if sys.version_info.major != 2:
+    sys.stderr.write('failure\tMust use Python 2 to load pysam.\n')
+    sys.exit(1)
 import os
+import random
+import pysam
 
 if __name__  == '__main__':
     
-    random.seed()
-    file1 = sys.argv[1]
-    file2 = sys.argv[2]
-    new_loc = os.getcwd()
-    # For testing purposes:
-    # file1 = r'/mnt/lustre/home/jdblischak/tag/sequences/JUNB/19238_C/seq.quality.sorted.nodup.bam'
-    # file2 = r'/mnt/lustre/home/jdblischak/tag/sequences/JUNB/18358_C/seq.quality.sorted.nodup.bam'
-    # For chimp:
-    # file2 = r'/mnt/lustre/data/share/HCR_Chipseq/Mapped/Input/Input_C_Pooled36_111206.quality.sort.nodup.bam'
-    # For human:
-    # file2 = '/mnt/lustre/data/share/HCR_Chipseq/Mapped/Input/Input_H_Pooled36_111206.quality.sort.nodup.bam'
-    # new_loc = r'/mnt/lustre/home/jdblischak/tag/sequences/JUNB/19238_C/'
+    seed = int(sys.argv[1])
+    num = int(sys.argv[2])
+    outdir = sys.argv[3]
+    filename = sys.argv[4]
 
-    sys.stderr.write('Reading in bam files...\n')
-    sam1 = pysam.Samfile(file1, 'rb')
-    sam2 = pysam.Samfile(file2, 'rb')
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+
+    if not os.path.exists(filename):
+        sys.stderr.write('failure\t%s\tdoes not exist.\n'%(filename))
+        sys.exit(1)
     
-    if sam1.mapped > sam2.mapped:
-         larger = sam1
-         smaller = sam2
-    else:
-         larger = sam2
-         smaller = sam1
-    sys.stderr.write('%s has more reads than %s.\n%d > %d\n'%(larger.filename,
-                     smaller.filename, int(larger.mapped), int(smaller.mapped)))
+    bam = pysam.Samfile(filename, 'rb')
 
-    total = int(smaller.mapped)
-    indices = list(xrange(larger.mapped))
+    total_count = bam.mapped + bam.unmapped
+    if total_count < num:
+        sys.stderr.write('failure\t%s\tcontains %d reads, which is less than requested: %d\n'%(
+            filename, total_count, num))
+        sys.exit(1)
+    
+    # Subsample num indices
+    indices = list(xrange(total_count))
+    random.seed(seed)
     random.shuffle(indices)
-    subbed = indices[0:total]
+    subbed = indices[0:num]
     subbed.sort()
     
-    sys.stderr.write('Outputting subsampled bam file...\n')
-    output_name = new_loc + '/' + larger.filename.split('/')[-1].strip('bam') + 'sub.' + str(smaller.mapped) + '.bam'
-    output = pysam.Samfile(output_name, 'wb', template = larger)
+    output_name = outdir + '/' + os.path.basename(filename).rstrip('bam') + \
+                  str(num) + '.bam'
+    output = pysam.Samfile(output_name, 'wb', template = bam)
     index = 0
     list_index = 0
-    for read in larger.fetch():
+    for read in bam.fetch(until_eof = True):
         if index == subbed[list_index]:
             output.write(read)
             list_index += 1
-            sys.stderr.write(str(list_index) + '\n')
-            if list_index == total:
+            #sys.stderr.write(str(list_index) + '\n')
+            if list_index == num:
                 break
         index += 1
     output.close()
     
     pysam.index(output_name)
-    output = pysam.Samfile(output_name, 'rb')    
-    if smaller.mapped != output.mapped:
-        sys.stderr.write('Mistake. Reads in smaller dataset: %d. Reads in subsampled dataset: %d\n'%(smaller.mapped, output.mapped))
+    # Test
+    test = pysam.Samfile(output_name, 'rb')    
+    if test.mapped + test.unmapped != num:
+        sys.stderr.write('failure\t%s\nObserved: %d\n Expected: %d\n'%(
+            output_name, test.mapped + test.unmapped, num))
+        sys.exit(1)
     else:
-        sys.stderr.write('Finished successfully.\nWrote %s\n'%(output.filename))
+        sys.stderr.write('success\t%s\n'%(test.filename))
     
-    sam1.close()
-    sam2.close()
-    output.close()
-    
-         
-    
-    
+    test.close()
