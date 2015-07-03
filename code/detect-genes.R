5b4fc0f15e72119a8b93a0f17406cb44a006f1ff
John Blischak <jdblischak@gmail.com>
2015-07-03 15:02:39 -0500

    Use a minumum number of cells for detection instead of percentage so that there is a clear trend when including more cells. Also focus subsampling on lower number of reads and cells since this is where the differences are likely to be.

--- code/detect-genes.R
+++ code/detect-genes.R
@@ -8,8 +8,8 @@ detect-genes.R [options] <num_cells> <seed> <exp>
 Options:
   -h --help              Show this screen.
   --individual=<ind>     Only use data from ind, e.g. 19098
-  --count=<x>            The minimum count required for detection [default: 1]
-  --fraction=<x>         The minimum fraction of cells required for detection [default: 0.5]
+  --min_count=<x>        The minimum count required for detection [default: 1]
+  --min_cells=<x>        The minimum number of cells required for detection [default: 1]
 
 Arguments:
   num_cells     number of single cells to subsample
@@ -18,8 +18,8 @@ Arguments:
 
 suppressMessages(library("docopt"))
 
-main <- function(num_cells, seed, exp_fname, individual = NULL, count = 1,
-                 fraction = 0.5) {
+main <- function(num_cells, seed, exp_fname, individual = NULL, min_count = 1,
+                 min_cells = 1) {
   # Load expression data
   exp_dat <- read.table(exp_fname, header = TRUE, sep = "\t",
                         stringsAsFactors = FALSE)
@@ -50,30 +50,29 @@ main <- function(num_cells, seed, exp_fname, individual = NULL, count = 1,
   exp_dat <- exp_dat[, sample(1:ncol(exp_dat), size = num_cells)]
 #   exp_dat[1:10, 1:10]
 #   dim(exp_dat)
-  detected <- apply(exp_dat, 1, detect_expression, count = count, fraction = fraction)
+  detected <- apply(exp_dat, 1, detect_expression, min_count = min_count, min_cells = min_cells)
   num_detected <- sum(detected)
 
   # Output
   cat(sprintf("%d\t%d\t%d\n", num_cells, seed, num_detected))
 }
 
-detect_expression <- function(x, count, fraction) {
+detect_expression <- function(x, min_count, min_cells) {
   # x - vector of counts
-  # count - minumum required observations per cell
-  # fraction - minimum required fraction of cells
-  expressed <- sum(x >= count)
-  fraction_expressed <- expressed / length(x)
-  return(fraction_expressed >= fraction)
+  # min_count - minumum required observations per cell
+  # min_cells - minimum required number of cells
+  expressed <- sum(x >= min_count)
+  return(expressed >= min_cells)
 }
 
 library("testit")
 assert("Detect expression function works properly.",
-       detect_expression(c(0, 0, 1, 1), 1, 0.5) == TRUE,
-       detect_expression(c(0, 0, 0, 1), 1, 0.5) == FALSE,
-       detect_expression(c(0, 0, 1, 1), 1, 0.75) == FALSE,
-       detect_expression(c(0, 1, 1, 1), 1, 0.75) == TRUE,
-       detect_expression(c(0, 2, 3, 1), 2, 0.5) == TRUE,
-       detect_expression(c(0, 1, 3, 1), 2, 0.5) == FALSE)
+       detect_expression(c(0, 0, 1, 1), 1, 2) == TRUE,
+       detect_expression(c(0, 0, 0, 1), 1, 2) == FALSE,
+       detect_expression(c(0, 0, 1, 1), 1, 3) == FALSE,
+       detect_expression(c(0, 1, 1, 1), 1, 3) == TRUE,
+       detect_expression(c(0, 2, 3, 1), 2, 2) == TRUE,
+       detect_expression(c(0, 1, 3, 1), 2, 2) == FALSE)
 
 if (!interactive() & getOption('run.main', default = TRUE)) {
   opts <- docopt(doc)
@@ -81,8 +80,8 @@ if (!interactive() & getOption('run.main', default = TRUE)) {
   main(num_cells = as.numeric(opts$num_cells),
        seed = as.numeric(opts$seed),
        exp_fname = opts$exp,
-       count <- as.numeric(opts$count),
-       fraction <- as.numeric(opts$fraction),
+       min_count = as.numeric(opts$min_count),
+       min_cells = as.numeric(opts$min_cells),
        individual = opts$individual)
 } else if (interactive() & getOption('run.main', default = TRUE)) {
   # what to do if interactively testing
@@ -90,6 +89,6 @@ if (!interactive() & getOption('run.main', default = TRUE)) {
        seed = 1,
        exp_fname = "/mnt/gluster/data/internal_supp/singleCellSeq/subsampled/molecule-counts-200000.txt",
        individual = "19098",
-       count = 10,
-       fraction = 0.5)
+       min_count = 10,
+       min_cells = 5)
 }
