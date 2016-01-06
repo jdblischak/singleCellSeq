925e01b03659be16341409f0d586cd4c780a97de
John Blischak <jdblischak@gmail.com>
2016-01-06 14:38:17 -0600

    Perform strand-specific read counting and update description.

--- code/count-reads-per-gene.sh
+++ code/count-reads-per-gene.sh
@@ -21,4 +21,11 @@ then
 fi
 
 echo "Counting reads per gene..."
-featureCounts -a $EXONS -F SAF -R -o $OUTDIR/$BASE.genecounts.txt $FILE
+featureCounts -a $EXONS -F SAF -R -s 1 -o $OUTDIR/$BASE.genecounts.txt $FILE
+
+# Bug introduced in 1.5 series. In 1.4 series, the "-R" file was the input
+# file with .featureCounts appended. Now it writes to the working directory
+# and concatenates the path to the beginning of the filename.
+# https://groups.google.com/forum/#!topic/subread/BjVCMxECE3o
+# I move it to the output directory since that makes more sense anyways.
+mv *$BASE.bam.featureCounts $OUTDIR/$BASE.bam.featureCounts
