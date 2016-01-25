48bcd05e6760886b6ca17cb9da28abdc74f84250
jhsiao999 <joyce.hsiao1@gmail.com>
2016-01-25 10:58:44 -0600

    edit variable names

--- code/permute-cv-compute-pval.r
+++ code/permute-cv-compute-pval.r
@@ -4,8 +4,8 @@ load("/home/joycehsiao/scratch-midway/single-cell/permute-cv/permute-cv-test.rda
 load("/home/joycehsiao/scratch-midway/single-cell/permute-cv/adj-cv.rda")
 load("/home/joycehsiao/scratch-midway/single-cell/permute-cv/permuted-distance.rda")
 
-mad <- do.call(cbind, lapply(permuted_distance, "[[", 1))
+mad_permute <- do.call(cbind, lapply(permuted_distance, "[[", 1))
 
-permuted_pval <- data.frame(squared_dev = rowMeans(mad > mad))
+permuted_pval <- data.frame(mad_pval = rowMeans(mad_permute > mad))
 save(permuted_pval, 
 	file = "/home/joycehsiao/scratch-midway/single-cell/permute-cv/permuted-pval.rda")
