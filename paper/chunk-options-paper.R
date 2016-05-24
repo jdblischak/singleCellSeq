096d956eae319bb4e3352aa06aa2b6ddb582cab2
Po Yuan Tung <ptung@uchicago.edu>
2016-05-24 16:18:20 -0500

    name the figures

--- paper/chunk-options-paper.R
+++ paper/chunk-options-paper.R
@@ -6,17 +6,27 @@ opts_chunk$set(include = FALSE)
 
 # Numbering for main figures
 fig_main_qc <- "1"
-fig_main_cpm <- "2"
-fig_main_subsample <- "3"
+fig_main_subsample <- "2"
+fig_main_batch <- "3"
 fig_main_normalization <- "4"
 fig_main_noise <- "5"
+fig_main_noisygene <- "6"
 
 # Numbering for supplementary figures
-fig_supp_flowcell <- "S1"
-fig_supp_sickle <- "SX"
+fig_supp_qc <- "S1"
+fig_supp_lda <- "S2"
+fig_supp_cellcycle <- "S3"
+fig_supp_subsample <- "S4"
+fig_supp_variance <- "S5"
+fig_supp_dropout <- "S6"
+fig_supp_permutation <- "S7"
+fig_supp_plurigene <- "S8"
+fig_supp_design <- "S9"
+fig_supp_proportion <- "S10"
+fig_supp_CV <- "S11"
 
 # Numbering for supplementary tables
-table_supp_genotypes <- "1"
+table_supp_collection <- "1"
 
 # Import data
 anno_filter <- read.table("../data/annotation-filter.txt", header = TRUE,
